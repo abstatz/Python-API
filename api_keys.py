8e72f3ef2a89159f0faba97e80ec6e1a3a6e94e4
Andrew <abstatz@gmail.com>
2020-09-30 20:57:35 -0700

    Homeowork

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "54be7231de11a28ba3dad864d13aed2f"
+
+# Google API Key
+g_key = "AIzaSyDxafF6gJlLNzOjy0k2RSGClj7xcdiksP8"
---
+++ config.py
@@ -0,0 +1,2 @@
+[DEFAULT]
+api_key = 54be7231de11a28ba3dad864d13aed2f
